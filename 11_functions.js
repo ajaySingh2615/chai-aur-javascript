ce591a849c7b82ed4cd362ac2c17dac9532557b5
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-04-23 23:54:03 +0530

    add initial implementation of potion brewing functions and logging

---
+++ 11_functions.js
@@ -0,0 +1,81 @@
+console.log(brewPotion("Healing Herbs", 3));
+
+function brewPotion(ingredient, dose) {
+  return `Brewing potion with${ingredient} (x${dose})... Potion ready`;
+}
+
+const mixElixir = function (ingredient) {
+  return `Mixing elexir with ${ingredient}`;
+};
+
+// no own 'this' no arguments object
+const distilEssence = (ingredient) => {
+  return `Mixing elexir with ${ingredient}`;
+};
+
+function oldBrewingLogs() {
+  console.log("Type: ", typeof arguments);
+  console.log("Is Array: ", Array.isArray(arguments));
+  const argsArray = Array.from(arguments);
+  console.log(argsArray);
+}
+
+oldBrewingLogs("Sage", "rosemary");
+
+// const arrowBrew = () => {
+//   try {
+//     console.log(arguments);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+// arrowBrew();
+
+let globalCount = 0;
+// impure function
+function brewAndCount(name) {
+  globalCount++;
+}
+
+// HOF
+function anotherFunctionForClass(brewAndCount) {
+  return function newBrew() {
+    // do something
+  };
+}
+
+// IIFE
+// ()()
+(function () {})();
+(() => {})();
+const potionShop = (function () {
+  let inventory = 0;
+
+  return {
+    brew() {
+      inventory++;
+      return `Brew potion #${inventory}`;
+    },
+
+    getStock() {
+      return inventory;
+    },
+  };
+})();
+
+console.log(potionShop);
+console.log(potionShop.brew());
+console.log(potionShop.inventory);
+
+// closure
+
+function makeFun() {
+  const name = "Mozilla";
+  function displayName() {
+    console.log(name);
+  }
+  return displayName;
+}
+
+const myFunc = makeFun();
+myFunc();
