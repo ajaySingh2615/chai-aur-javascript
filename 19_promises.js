910b82449653a5f9e3c95fcd6f7c1d32f63df85a
ajaySingh2615 <ajaysingh261526@gmail.com>
2026-04-28 10:18:21 +0530

    add callback and promise-based order processing functions for improved order management

---
+++ 19_promises.js
@@ -0,0 +1,60 @@
+function prepareOrderCB(dish, cb) {
+  setTimeout(() => cb(null, { dish, status: "Prepared Dish" }), 100);
+}
+function pickUpOrderCB(order, cb) {
+  setTimeout(() => cb(null, { ...order, status: "Picked-up" }), 100);
+}
+function deliveredOrderCB(order, cb) {
+  setTimeout(() => cb(null, { ...order, status: "Delivered" }), 100);
+}
+
+prepareOrderCB("Biryani", (err, order) => {
+  if (err) return console.log(err);
+  pickUpOrderCB(order, (err, order) => {
+    if (err) return console.log(err);
+    deliveredOrderCB(order, (err, order) => {
+      if (err) return console.log(err);
+      console.log(`${order.dish}: ${order.status}`);
+    });
+  });
+});
+
+// pending, fulfilled, rejected
+//
+function prepareOrder(dish) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!dish) {
+        reject(new Error("No dish is there"));
+        return;
+      }
+      console.log(`${dish} is ready`);
+      resolve({ dish, status: "prepared" });
+    }, 100);
+  });
+}
+function pickupOrder(order) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!order) {
+        reject(new Error("No order is there"));
+        return;
+      }
+      console.log(`${order} is ready`);
+      resolve({ ...order, status: "pickedUp" });
+    }, 100);
+  });
+}
+function deliverOrder(order) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`${order} is ready`);
+      resolve({ ...order, status: "delivered" });
+    }, 100);
+  });
+}
+
+prepareOrder("Panner Tikka")
+  .then((order) => pickUpOrder(order))
+  .then((order) => deliverOrder(order))
+  .catch((err) => console.log(err));
